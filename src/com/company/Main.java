3bb09c26f0e05a662a31a524a184f9c338d64058
Daniel Kaczorowski <kaczorowski.daniel@gmail.com>
2016-01-20 00:18:07 +0100

    v3.01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Scanner;
 
 /**
  * Temat: Wyznaczania średniej arytmetycznej, odchylenia standardowego i wariancji ciągu n liczb oraz min i max.
@@ -9,49 +8,64 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Start programu.");
-        System.out.println("");
-        //Tworze tablice z ciagiem.
-        int[] ciag = {1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 33, 44, 55, 66, 77, 88, 99, 100};
-        int i;
-        int ciagWynik = 0;
-        double srednia;
-        //Tworze instrukcje obliczajaca rekurencyjnie sume ciagu.
-        for (i = 0; i < ciag.length; i++) {
-            ciagWynik = ciagWynik + ciag[i];
-        }
-        //Obliczam srednia arytmetyczna
-        srednia = ciagWynik / ciag.length;
-        //Szukam minimalna wartosc ciagu
-        int minWynik = ciag[0];
-        for (int j = 1; j < ciag.length; j++)
-            if (minWynik > ciag[j]) {
-                minWynik = ciag[j];
-            }
-        //Szukam maksymalnej wartosci ciagu
-        int maxWynik = ciag[0];
-        for (int n = 1; n < ciag.length; n++)
-            if (maxWynik < ciag[n]) {
-                maxWynik = ciag[n];
-            }
-        //Wariancja ciagu
-        /**
-         *
-         */
-        //Odchylenie standardowe
-        /**
-         * double std = 0.0;
-         * double avg = mean(vals);
-         *
-         * for (int i=0; i< vals.length; i++)
-         *     std += (vals[i]-avg)*(vals[i]-avg);
-         *
-         *     std/=vals.length;
-         *     std=Math.sqrt(std)
-         *
-         *     return std;
-         */
-        System.out.println("Suma wszystkich wyrazow ciagu wynosi: " + ciagWynik);
+    }
+}
+
+
+
+/**
+ *         /**System.out.println("Start programu.");
+ System.out.println("");
+ //Tworze tablice z ciagiem.
+ int[] ciag = {1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 33, 44, 55, 66, 77, 88, 99, 100};
+ int i;
+ int ciagWynik = 0;
+ double srednia;
+ //Tworze instrukcje obliczajaca rekurencyjnie sume ciagu.
+ for (i = 0; i < ciag.length; i++) {
+ ciagWynik = ciagWynik + ciag[i];
+ }
+ //Obliczam srednia arytmetyczna
+ srednia = ciagWynik / ciag.length;
+ //Szukam minimalna wartosc ciagu
+ int minWynik = ciag[0];
+ for (int j = 1; j < ciag.length; j++)
+ if (minWynik > ciag[j]) {
+ minWynik = ciag[j];
+ }
+ //Szukam maksymalnej wartosci ciagu
+ int maxWynik = ciag[0];
+ for (int n = 1; n < ciag.length; n++)
+ if (maxWynik < ciag[n]) {
+ maxWynik = ciag[n];
+ }
+ //Wariancja ciagu
+ private static double wariancjaWynik(double[] ciag) {
+ double wariancja;
+ double sum = 0.0;
+ double srednia =;
+ for (double l : ciag) {
+ sum = sum + (l - srednia) * (l - srednia);
+ }
+ wariancja=sum/(ciag.length-1);
+ return wariancja;
+ }
+ /**
+ *
+ */
+//Odchylenie standardowe
+/**
+ * double std = 0.0;
+ * double avg = mean(vals);
+ *
+ * for (int i=0; i< vals.length; i++)
+ *     std += (vals[i]-avg)*(vals[i]-avg);
+ *
+ *     std/=vals.length;
+ *     std=Math.sqrt(std)
+ *
+ *     return std;
+System.out.println("Suma wszystkich wyrazow ciagu wynosi: " + ciagWynik);
         System.out.println("Ilosc wyrazow w ciagu wynosi: " + ciag.length);
         System.out.println("Srednia arytmetyczna ciagu wynosi: " + srednia);
         System.out.println("Odchylenie Standardowe wynosi: ");
@@ -60,10 +74,12 @@ public class Main {
         System.out.println("Wartosc maxymalna ciagu to: " + maxWynik);
         System.out.println("");
         System.out.println("Koniec programu.");
-    }
-}
-
-/**
+        }
+private static double wariancja(double[] liczby){
+        double wariancja;
+        double srednia = ;
+        }
+        }
  * System.out.println("Start pogramu.");
  * System.out.println("Wybierz jaki rodzaj ciągu chcesz uzyskać.");
  * System.out.println("1)Ciąg arytmetyczny.");
@@ -88,4 +104,6 @@ public class Main {
  * }
  * else System.out.println("Wybrana pozycja nie istnieje!");
  * System.out.println("Koniec programu.");
- */
+
+
+        */
